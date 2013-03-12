6ef08c536e4ce9ceeac5d3a1f3280e620056e03d
David Tee <davidkmo@gmail.com>
2013-03-12 11:41:17 -0700

    Change solr receipe to ignore direction creation if cores are empty. Cookbook user can use this to load custom schema. Removed pre-stop, seem like it's not necressary.

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,6 +5,7 @@ default['solr']['server_dir'] = "/opt/solr"
 default['solr']['log_dir'] = "/service/logs"
 default['solr']['config_dir'] = "/etc/solr"
 default['solr']['data_dir'] = "/var/lib/solr"
+default['solr']['stop_port'] = 8079
 
 default['solr']['user'] = 'root'
 default['solr']['group'] = 'root'
--- recipes/default.rb
+++ recipes/default.rb
@@ -22,58 +22,61 @@ bash 'unpack solr' do
 end
 
 # Configure solr
-directory node['solr']['config_dir'] do
-	recursive true
-	action :create
-end
 
-template "#{node.solr.config_dir}/logging.properties" do
-	source "logging.properties.erb"
-	owner node['solr']['user']
-	group node['solr']['group']
-	variables(
-		:log_dir             => node['solr']['log_dir']
-	)
-	mode 0744
-end
-
-template "#{node.solr.config_dir}/solr.xml" do
-	source "solr.xml.erb"
-	owner node['solr']['user']
-	group node['solr']['group']
-	variables(
-		:cores             => node['solr']['cores']
-	)
-	mode 0744
-end
-
-node['solr']['cores'].each do |name|
-	directory "#{node.solr.config_dir}/#{name}/conf" do
-		owner node['solr']['user']
-		group node['solr']['group']
-		mode '0744'
+if not node['solr']['cores'].empty?
+	directory node['solr']['config_dir'] do
 		recursive true
+		action :create
 	end
 
-	template "#{node.solr.config_dir}/#{name}/conf/schema.xml" do
-		source "schema.xml.erb"
+	template "#{node.solr.config_dir}/logging.properties" do
+		source "logging.properties.erb"
 		owner node['solr']['user']
 		group node['solr']['group']
 		variables(
-			:name             => name
+			:log_dir             => node['solr']['log_dir']
 		)
-		mode 0744
+		mode 0644
 	end
 
-	template "#{node.solr.config_dir}/#{name}/conf/solrconfig.xml" do
-		source "solrconfig.xml.erb"
+	template "#{node.solr.config_dir}/solr.xml" do
+		source "solr.xml.erb"
 		owner node['solr']['user']
 		group node['solr']['group']
 		variables(
-			:name             => name,
-			:data_dir         => "#{node.solr.data_dir}/#{name}",
+			:cores             => node['solr']['cores']
 		)
-		mode 0744
+		mode 0644
+	end
+
+	node['solr']['cores'].each do |name|
+		directory "#{node.solr.config_dir}/#{name}/conf" do
+			owner node['solr']['user']
+			group node['solr']['group']
+			mode '0644'
+			recursive true
+		end
+
+		template "#{node.solr.config_dir}/#{name}/conf/schema.xml" do
+			source "schema.xml.erb"
+			owner node['solr']['user']
+			group node['solr']['group']
+			variables(
+				:name             => name
+			)
+			mode 0644
+		end
+
+		template "#{node.solr.config_dir}/#{name}/conf/solrconfig.xml" do
+			source "solrconfig.xml.erb"
+			owner node['solr']['user']
+			group node['solr']['group']
+			variables(
+				:name             => name,
+				:data_dir         => "#{node.solr.data_dir}/#{name}",
+			)
+			mode 0644
+		end
 	end
 end
 
@@ -83,13 +86,14 @@ template "/etc/init/solr.conf" do
 	owner "root"
 	group "root"
 	variables(
+		:stop_port           => node['solr']['stop_port'],
 		:server_dir          => node['solr']['server_dir'],
 		:log_dir             => node['solr']['log_dir'],
 		:config_dir          => node['solr']['config_dir'],
 		:version             => node['solr']['version'],
 		:path                => "#{node['solr']['server_dir']}/solr-#{node['solr']['version']}/example",
 	)
-	mode 0744
+	mode 0644
 end
 
 service "solr" do
